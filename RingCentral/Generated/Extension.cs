25715304251ffad2cb39717a4738b93d5dfab690
Tyler Long <tyler4long@gmail.com>
2016-08-03 15:22:41 +0800

    Extension Put

--- RingCentral/Generated/Extension.cs
+++ RingCentral/Generated/Extension.cs
@@ -437,5 +437,131 @@ namespace RingCentral
                 public string reason { get; set; }
             }
         }
+        public Task<PutResponse> Put(object requestBody)
+        {
+            return RC.Put<PutResponse>(Endpoint(true), requestBody, null);
+        }
+        public class PutResponse
+        {
+            public string id { get; set; }
+            public string uri { get; set; }
+            public Contact contact { get; set; }
+            public Departments departments { get; set; }
+            public string extensionNumber { get; set; }
+            public string name { get; set; }
+            public string partnerId { get; set; }
+            public Permissions permissions { get; set; }
+            public ProfileImage profileImage { get; set; }
+            public Reference[] references { get; set; }
+            public RegionalSettings regionalSettings { get; set; }
+            public ServiceFeature[] serviceFeatures { get; set; }
+            public string setupWizardState { get; set; }
+            public string status { get; set; }
+            public StatusInfo statusInfo { get; set; }
+            public string type { get; set; }
+            public class Contact
+            {
+                public string firstName { get; set; }
+                public string lastName { get; set; }
+                public string company { get; set; }
+                public string email { get; set; }
+                public string businessPhone { get; set; }
+                public BusinessAddress businessAddress { get; set; }
+                public class BusinessAddress
+                {
+                    public string country { get; set; }
+                    public string state { get; set; }
+                    public string city { get; set; }
+                    public string street { get; set; }
+                    public string zip { get; set; }
+                }
+            }
+            public class Departments
+            {
+                public string id { get; set; }
+                public string uri { get; set; }
+                public string extensionNumber { get; set; }
+            }
+            public class Permissions
+            {
+                public Admin admin { get; set; }
+                public InternationalCalling internationalCalling { get; set; }
+                public class Admin
+                {
+                    public bool? enabled { get; set; }
+                }
+                public class InternationalCalling
+                {
+                    public bool? enabled { get; set; }
+                }
+            }
+            public class ProfileImage
+            {
+                public string uri { get; set; }
+                public string etag { get; set; }
+                public string lastModified { get; set; }
+                public string contentType { get; set; }
+                public string[] scales { get; set; }
+            }
+            public class Reference
+            {
+                public string @ref { get; set; }
+                public string type { get; set; }
+            }
+            public class RegionalSettings
+            {
+                public HomeCountry homeCountry { get; set; }
+                public Timezone timezone { get; set; }
+                public Language language { get; set; }
+                public GreetingLanguage greetingLanguage { get; set; }
+                public FormattingLocale formattingLocale { get; set; }
+                public class HomeCountry
+                {
+                    public string id { get; set; }
+                    public string uri { get; set; }
+                    public string name { get; set; }
+                }
+                public class Timezone
+                {
+                    public string id { get; set; }
+                    public string uri { get; set; }
+                    public string name { get; set; }
+                    public string description { get; set; }
+                }
+                public class Language
+                {
+                    public string id { get; set; }
+                    public string uri { get; set; }
+                    public bool? greeting { get; set; }
+                    public bool? formattingLocale { get; set; }
+                    public string localeCode { get; set; }
+                    public string name { get; set; }
+                    public bool? ui { get; set; }
+                }
+                public class GreetingLanguage
+                {
+                    public string id { get; set; }
+                    public string localeCode { get; set; }
+                    public string name { get; set; }
+                }
+                public class FormattingLocale
+                {
+                    public string id { get; set; }
+                    public string localeCode { get; set; }
+                    public string name { get; set; }
+                }
+            }
+            public class ServiceFeature
+            {
+                public bool? enabled { get; set; }
+                public string featureName { get; set; }
+                public string reason { get; set; }
+            }
+            public class StatusInfo
+            {
+                public string comment { get; set; }
+                public string reason { get; set; }
+            }
+        }
     }
 }
