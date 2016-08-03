1222e101e2a9dee571f0fb75cf6648ba35a212ac
Tyler Long <tyler4long@gmail.com>
2016-08-03 12:12:25 +0800

    Add response model for ServiceInfo

--- RingCentral/Generated/ServiceInfo.cs
+++ RingCentral/Generated/ServiceInfo.cs
@@ -12,5 +12,53 @@ namespace RingCentral
                 return "service-info";
             }
         }
+        public class MeetingServiceInfo
+        {
+            public string uri { get; set; }
+            public string supportUri { get; set; }
+            public string intlDialInNumbersUri { get; set; }
+            public ExternalUserInfo externalUserInfo { get; set; }
+            public DialInNumbers dialInNumbers { get; set; }
+            public class ExternalUserInfo
+            {
+                public string accountId { get; set; }
+                public string userId { get; set; }
+                public string userToken { get; set; }
+                public int? userType { get; set; }
+            }
+            public class DialInNumbers
+            {
+                public string phoneNumber { get; set; }
+                public string formattedNumber { get; set; }
+                public string location { get; set; }
+                public Country country { get; set; }
+                public class Country
+                {
+                    public string id { get; set; }
+                    public string uri { get; set; }
+                    public string callingCode { get; set; }
+                    public string isoCode { get; set; }
+                    public string name { get; set; }
+                }
+            }
+        }
+        public class AccountServiceInfo
+        {
+            public string uri { get; set; }
+            public string servicePlanName { get; set; }
+            public ServiceFeature[] serviceFeatures { get; set; }
+            public Limit[] limits { get; set; }
+            public class ServiceFeature
+            {
+                public string featureName { get; set; }
+                public bool? enabled { get; set; }
+            }
+            public class Limit
+            {
+                public int? freeSoftPhoneLinesPerExtension { get; set; }
+                public int? meetingSize { get; set; }
+                public int? maxMonitoredExtensionsPerUser { get; set; }
+            }
+        }
     }
 }
