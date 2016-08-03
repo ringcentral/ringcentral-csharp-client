4d77ad04f946faef461f5a5ea083828ff5c586e0
Tyler Long <tyler4long@gmail.com>
2016-08-03 15:07:44 +0800

    Get ServiceInfo working

---
+++ RingCentral/Special/ServiceInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Tasks;
+
+namespace RingCentral
+{
+    public partial class ServiceInfo
+    {
+        public Task<T> Get<T>()
+        {
+            return RC.Get<T>(Endpoint(true), null);
+        }
+    }
+}
