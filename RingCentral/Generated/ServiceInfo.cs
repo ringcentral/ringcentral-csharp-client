93386098971c6eb25eebf7e8582b5cf442268a54
Tyler Long <tyler4long@gmail.com>
2016-08-11 09:29:34 +0800

    Add AccountServiceInfo

--- RingCentral/Generated/ServiceInfo.cs
+++ RingCentral/Generated/ServiceInfo.cs
@@ -25,5 +25,16 @@ namespace RingCentral
             // Dial-in numbers data
             public DialInNumbers dialInNumbers { get; set; }
         }
+        public partial class AccountServiceInfo
+        {
+            // Canonical URI of the account Service Info resource
+            public string uri { get; set; }
+            // Account Service Plan name
+            public string servicePlanName { get; set; }
+            // Service features information, see Service Feature List
+            public ServiceFeatureInfo[] serviceFeatures { get; set; }
+            // Limits which are effective for the account
+            public AccountLimits limits { get; set; }
+        }
     }
 }
