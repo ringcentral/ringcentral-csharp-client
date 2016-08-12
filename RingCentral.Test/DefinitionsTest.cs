95c65e7e4aa1951cb151adbba55b72c471410ce6
Tyler Long <tyler4long@gmail.com>
2016-08-12 11:32:40 +0800

    Add DefinitionsTest

---
+++ RingCentral.Test/DefinitionsTest.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace RingCentral.Test
+{
+    public class DefinitionsTest
+    {
+        [Fact]
+        public void AllTheDefinitions()
+        {
+            var rc = new RestClient("", "");
+            var restApi = rc.Restapi();
+            var account = restApi.Account();
+            var dict = restApi.Dictionary();
+
+            Assert.NotNull(account);
+            Assert.NotNull(dict);
+        }
+    }
+}
