8b93468bf5b6ef16956f189e799c1415ecd2e797
Tyler Long <tyler4long@gmail.com>
2016-08-24 17:16:35 +0800

    Test dialing-plan

---
+++ RingCentral.Test/DialingPlanTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+using Xunit;
+
+namespace RingCentral.Test
+{
+    [Collection("RestClient collection")]
+    public class DialingPlanTest : IDisposable
+    {
+        private RestClient rc;
+        public DialingPlanTest(RestClientFixture fixture)
+        {
+            rc = fixture.rc;
+        }
+
+        [Fact]
+        public void GetDialingPlans()
+        {
+            var account = rc.Restapi().Account();
+            var result = account.DialingPlan().List().Result;
+            Assert.NotNull(result);
+            Assert.NotEmpty(result.records);
+        }
+
+        public void Dispose()
+        {
+            Thread.Sleep(5000);
+        }
+    }
+}
