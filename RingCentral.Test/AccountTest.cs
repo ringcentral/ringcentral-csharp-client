a202b83f44aba3d04de849e76ce6bb66d10509b2
Tyler Long <tyler4long@gmail.com>
2016-08-26 17:50:24 +0800

    Add AccountTest

---
+++ RingCentral.Test/AccountTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using Xunit;
+
+namespace RingCentral.Test
+{
+    [Collection("RestClient collection")]
+    public class AccountTest : IDisposable
+    {
+        private RestClient rc;
+        public AccountTest(RestClientFixture fixture)
+        {
+            rc = fixture.rc;
+        }
+
+        [Fact]
+        public void GetAccount()
+        {
+            var account = rc.Restapi().Account().Get().Result;
+            Assert.NotNull(account);
+            Assert.NotNull(account.serviceInfo);
+        }
+        public void Dispose()
+        {
+            Thread.Sleep(5000);
+        }
+    }
+}
