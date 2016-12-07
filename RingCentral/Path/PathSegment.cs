dad700c3cce35c25c59950eca4c6e497fb47f20b
Tyler Long <tyler4long@gmail.com>
2016-12-07 10:59:08 +0800

    Create Path folder

--- RingCentral/Path/PathSegment.cs
+++ RingCentral/Path/PathSegment.cs
@@ -37,34 +37,4 @@ namespace RingCentral
             }
         }
     }
-
-    public class MockPathSegment : PathSegment
-    {
-        public MockPathSegment(RestClient rc) : base(null)
-        {
-            this.rc = rc;
-        }
-
-        protected override string Segment
-        {
-            get
-            {
-                return "";
-            }
-        }
-
-        public override string Endpoint(bool withId)
-        {
-            return "";
-        }
-
-        private RestClient rc;
-        protected override RestClient RC
-        {
-            get
-            {
-                return rc;
-            }
-        }
-    }
 }
