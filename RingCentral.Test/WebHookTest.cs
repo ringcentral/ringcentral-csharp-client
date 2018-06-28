6ae28566ea262cf2087de38b4163b152140abdbc
Tyler Long <tyler4long@gmail.com>
2018-06-28 12:00:59 +0800

    Add WebHook test

---
+++ RingCentral.Test/WebHookTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+using Flurl.Http;
+using Xunit;
+
+namespace RingCentral.Test
+{
+    [Collection("RestClient collection")]
+    public class WebHookTest : IDisposable
+    {
+        private RestClient rc;
+        public WebHookTest(RestClientFixture fixture)
+        {
+            rc = fixture.rc;
+        }
+
+        [Fact]
+        public async void SetupWebHook()
+        {
+            try
+            {
+                var r = await rc.Restapi().Subscription().Post(new CreateSubscriptionRequest
+                {
+                    eventFilters = new string[] { "/restapi/v1.0/account/~/extension/~/message-store" },
+                    deliveryMode = new NotificationDeliveryModeRequest
+                    {
+                        transportType = "WebHook",
+                        address = "http://www.example.com/webhook"
+                    }
+                });
+            } catch(FlurlHttpException fhe) {
+                string errorMessage = fhe.GetResponseString();
+                Console.WriteLine(errorMessage);
+                Assert.Equal("{\"errorCode\":\"SUB-521\",\"message\":\"WebHook is not reachable\",\"errors\":[{\"errorCode\":\"SUB-521\",\"message\":\"WebHook is not reachable\"}]}", errorMessage);
+            }
+        }
+
+        public void Dispose()
+        {
+            Thread.Sleep(100);
+        }
+    }
+}
