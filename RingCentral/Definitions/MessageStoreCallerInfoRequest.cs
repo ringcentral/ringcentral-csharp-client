c139b5fa333207d881618515710c093a6746f116
Weihui Guo <weihui.guo@outlook.com>
2019-07-10 13:45:20 -0400

    Adding name to cover page works, but not location
    public class CallerInfoRequest : MessageStoreCallerInfoRequest
            {
                public string name;
                public string location;
            }

--- RingCentral/Definitions/MessageStoreCallerInfoRequest.cs
+++ RingCentral/Definitions/MessageStoreCallerInfoRequest.cs
@@ -4,5 +4,9 @@ namespace RingCentral
     {
         // Phone number in E.164 format
         public string @phoneNumber { get; set; }
+        
+        public string @name { get; set; }
+        
+        public string @location { get; set; }
     }
 }
